da13f9e3599a07e77b77799b5c3df8f16396a2a9
jihye <jyebe9034@gmail.com>
2020-10-12 22:00:24 +0900

    함수 호출시 this 바인딩

--- basic/Function/FunctionCallAndThis/ThisBinding.js
+++ basic/Function/FunctionCallAndThis/ThisBinding.js
@@ -17,3 +17,14 @@ otherObject.sayName = myObject.sayName
 // sayName() 메서드 호출
 myObject.sayName() // foo
 otherObject.sayName() // bar
+
+// 함수를 호출할 때 this 바인딩을 보여주는 예제코드
+let test = 'this is test'
+console.log(window.test) // this is test
+
+// sayFoo() 함수
+let sayFoo = function () {
+  console.log(this.test) // this is test
+}
+
+sayFoo()
