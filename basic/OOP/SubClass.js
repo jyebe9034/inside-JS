561d8f8cab9a827a7941b073c57bcbd9920cd413
jyebe9034 <jyebe9034@gmail.com>
2020-12-01 22:42:11 +0900

    subClass 함수 생성 마무리

--- basic/OOP/SubClass.js
+++ basic/OOP/SubClass.js
@@ -2,12 +2,19 @@ let SuperClass = subClass(obj);
 let SubClass = SuperClass.subClass(obj);
 
 function subClass(obj) {
-
-    let parent = this;
+    let parent = this === window ? Function : this;
     let F = function() {};
 
     /* (1) 자식클래스 (함수 객체) 생성 */
     let child = function() {
+        let _parent = child.parent;
+
+        if (_parent && _parent !== Function) {
+            _parent.apply(this, arguments);
+        }
+        if (child.prototype.hasOwnProoerty('_init')) {
+            child.prototype._init.apply(this, arguments);
+        }
     };
 
     /* (2) 생성자 호출 */
@@ -15,10 +22,16 @@ function subClass(obj) {
     F.prototype = parent.prototype;
     child.prototype = new F();
     child.prototype.constructor = child;
-    child.parent = parent.prototype;
-    child.parent_constructor = parent;
+    child.parent = parent;
+    child.subClass = arguments.callee;
 
     /* (4) obj를 통해 들어온 변수 및 메서드를 자식 클래스에 추가 */
+    for (let i in obj) {
+        if (obj.hasOwnProoerty(i)) {
+            child.prototype[i] = obj[i];
+        }
+    }
+
     /* (5) 자식 함수 객체 반환 */
     return child;
 }
